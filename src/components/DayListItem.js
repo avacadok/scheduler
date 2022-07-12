6d0e7123342b3ae75b4ca3fe2717e8f4def2e006
avacadok <avakuang8@gmail.com>
2022-07-12 10:16:08 -0700

    building the DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DayListItem(props) {
+  const {setDay, name, spots} = props
+  return (
+    <li onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">{spots} spots remaining</h3>
+    </li>
+  );
+}
