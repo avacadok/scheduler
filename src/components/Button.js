ab0fa1fdfa76f25bc83c4081896bcf4b7e12c26a
avacadok <avakuang8@gmail.com>
2022-07-11 22:27:42 -0700

    use classnames library to help compose the list of classes for the button element

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-  let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm"
-  } 
-  
-  if (props.danger) {
-    buttonClass += " button--danger"
-  }
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  })
 
    return (
    <button 
@@ -19,5 +15,7 @@ export default function Button(props) {
    onClick={props.onClick}
    disabled={props.disabled}
    >
-     {props.children}</button>);
+     {props.children}
+     </button>
+  );
 }
