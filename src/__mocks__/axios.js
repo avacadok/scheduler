3b536532160521a7869fa43591b309d700973e99
avacadok <avakuang8@gmail.com>
2022-07-20 15:30:04 -0700

    adding delete to mock axios and test on application test file

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -83,7 +83,15 @@ export default {
       });
     }
   }),
+
   put: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
+  }),
+  
+  delete: jest.fn(url => {
     return Promise.resolve({
       status: 204,
       statusText: "No Content"
